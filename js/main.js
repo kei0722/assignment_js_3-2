bab2214a704e0d5beb7b6dcbabe5eb1eb6ba8f86
kei0722 <kayare919@gmail.com>
2020-07-23 05:36:52 +0900

    main.jsを関数などを使いスッキリするように修正

--- js/main.js
+++ js/main.js
@@ -4,55 +4,62 @@ const todoList = [];
 
 document.getElementById('add-btn').addEventListener('click', () => {
 
+	let inputValue = document.getElementById('input-task').value;
+
+	if (inputValue === '') {
+		return;
+	}
+
 	const tr = document.createElement('tr');
 	const statusButton = document.createElement('button');
 	const deleteButton = document.createElement('button');
 	const tbody = document.querySelector('tbody');
-	const addTr = tbody.appendChild(tr);
 
 	let todoListId = todoList.length;
 
 	const todo = {
 		id: todoListId,
-		comment: document.getElementById('input-task').value,
+		comment: inputValue,
 		status: '作業中',
 		deleteBtn: '削除',
 	};
 
-	todoList.push(todo);
-
-	const tdId = document.createElement('td');
-	const tdComment = document.createElement('td');
-	const tdStatus = document.createElement('td');
-	const tdDeleteBtn = document.createElement('td');
-
-	tdId.textContent = todo.id;
-	tdComment.textContent = todo.comment;
-
-	statusButton.textContent = todo.status;
-	tdStatus.appendChild(statusButton);
-
-	deleteButton.textContent = todo.deleteBtn;
-	deleteButton.className = 'delete-button';
-	tdDeleteBtn.appendChild(deleteButton);
-
-	addTr.appendChild(tdId);
-	addTr.appendChild(tdComment);
-	addTr.appendChild(tdStatus);
-	addTr.appendChild(tdDeleteBtn);
-
-	document.getElementById('input-task').value = "";
-
-	tdDeleteBtn.addEventListener('click', () => {
-		
-		todoList.splice(Number(todoListId.textContent), 1);
-		tbody.removeChild(tr);
-
+	function showTodoList() {
 		for (let i = 0; i < todoList.length; i++) {
 			todoList[i].id = i;
-			tdId.textContent = todoList[i].id;
-			tbody.children[i].firstChild.textContent = i;
+			tbody.children[i].children[0].textContent = i;
+			tbody.children[i].children[1].textContent = todoList[i].comment;
 		}
+	}
+	
+	function showButton() {
+		for (let i = 0; i < todoList.length; i++) {
+			statusButton.textContent = todoList[i].status;
+			tbody.children[i].children[2].appendChild(statusButton);
+			deleteButton.textContent = todoList[i].deleteBtn;
+			deleteButton.className = 'delete-button';
+			tbody.children[i].children[3].appendChild(deleteButton);
+		}
+	}
+
+	todoList.push(todo);
+
+	for (let i = 0; i < Object.keys(todo).length; i++) {
+		const td = document.createElement('td');
+		tbody.appendChild(tr).appendChild(td);
+	}
+	
+	showTodoList();
+	
+	showButton();
+
+	document.getElementById('input-task').value = '';
+
+	deleteButton.addEventListener('click', e => {
+		todoList.splice(Number(e.target.closest('tr').children[0].textContent), 1);
+		tbody.removeChild(e.target.closest('tr'));
+		showTodoList();
 	});
+
 });
 
